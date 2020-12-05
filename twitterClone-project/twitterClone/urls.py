d9b86217abe0ff886624134b8199408adc17f50f
hassan boulhilt <hboulhilt@gmail.com>
2020-12-05 19:19:38 +0100

    create html form for sign up

--- twitterClone-project/twitterClone/urls.py
+++ twitterClone-project/twitterClone/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from apps.core.views import frontpage
+from apps.core.views import frontpage, signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',frontpage,name="frontpage"),
+    path('signup',signup,name="signup"),
 ]

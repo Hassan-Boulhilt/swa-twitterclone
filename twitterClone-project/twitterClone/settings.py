c2c054923e3f6e6a367a4cbc094576213cf19b2f
hassan boulhilt <hboulhilt@gmail.com>
2020-12-08 21:46:52 +0100

    Add view feed and its template

--- twitterClone-project/twitterClone/settings.py
+++ twitterClone-project/twitterClone/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'feed'
 LOGOUT_REDIRECT_URL = 'frontpage'
 
 
--- twitterClone-project/twitterClone/urls.py
+++ twitterClone-project/twitterClone/urls.py
@@ -19,11 +19,18 @@ from django.contrib.auth import views
 
 
 from apps.core.views import frontpage, signup
-
+from apps.feed.views import feed
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', frontpage, name='frontpage'),
     path('signup/', signup, name='signup'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('login/', views.LoginView.as_view(template_name='core/login.html'), name="login"),
+    path('login/', views.LoginView.as_view(template_name='core/login.html'), name="login"),    
+
+    #
+    # Feed app's url path
+    # 
+
+    path('feed/',feed, name="feed"),
+      
 ]
